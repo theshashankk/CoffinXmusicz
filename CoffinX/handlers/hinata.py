83424ce24065cdbe192650ed0aa9ef523afa5ab8
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-23 14:12:48 +0530

    Update hinata.py

--- CoffinX/handlers/hinata.py
+++ CoffinX/handlers/hinata.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-from config import BOT_NAME as bn
+from CoffinX.config import BOT_NAME as bn
 
 
 
